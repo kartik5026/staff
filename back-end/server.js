57278738160afb125b803f257f7be5e9d5ab8b7b
kartik5026 <87748398+kartik5026@users.noreply.github.com>
2024-09-29 10:52:17 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://staff-frontend-final.vercel.app/',
     credentials:true
 
 }))
